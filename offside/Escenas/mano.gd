08a2c1c3aae85319a40700760b15dc5a0e117a20
Gary <tomasgarayzar@gmail.com>
2026-05-16 15:38:05 -0300

    script agregar cartas no anda pero esta la idea

--- offside/Escenas/mano.gd
+++ offside/Escenas/mano.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
-var cartas: Array = []
+var cartas : Array = []
 var cartas_max: int = 7
-
 
 @export var angulo: float = 30.0
 @export var spaciado: float = 120.0
+
+@export var carta_scene: PackedScene
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.keycode == KEY_SPACE and event.pressed:
+			agregar(carta_scene)
 
 func agregar(Carta : PackedScene) -> void:
 	var carti = Carta.instantiate()
@@ -24,4 +30,3 @@ func orden() -> void:
 		var Carta = cartas[i]
 		var offset = (i - (count - 1) / 2.0)
 		Carta.position.x = offset * spaciado
-		Carta.rotation = deg_to_rad(offset * angulo / count)
